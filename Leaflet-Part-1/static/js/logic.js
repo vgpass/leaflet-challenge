298c84f6894a04e4464b4efce699441d59e100a6
Vincent_Passanisi <vince@santafeimporters.com>
2023-02-05 20:49:56 -0800

    Updating config file

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,4 @@
+// Create the tile layer that will be the background of our map.
+var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
